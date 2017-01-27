622756553281a9c57cb8c45ee57b472cf0cf162c
GEORGE PILITSOLOU <george.pilitsoglou@gmail.com>
2017-01-27 12:34:14 +0000

    added scripts and dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -3,3 +3,5 @@ var paths = require('./gulp-config.js').paths;
 
 // path to separate tasks
 var requireDir = require('require-dir')('./gulp-tasks');
+
+gulp.task('build', ['build:vendor'], function () {});
